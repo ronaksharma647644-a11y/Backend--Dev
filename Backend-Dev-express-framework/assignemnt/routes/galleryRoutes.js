acda5c1bcbaa8f56d35c73819d9c22db77fa1763
ronaksharma647644-a11y <ronaksharma647644@gmail.com>
2026-02-10 22:05:27 +0530

    Remove express assignment from unix-commands-git-github branch

--- Backend-Dev-express-framework/assignemnt/routes/galleryRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  const images = ["img1.jpg", "img2.jpg"];
-  res.render("gallery", { images });
-});
-
-export default router;
